faf34283f44d0dc7391ad963c5125134e0198eed
Maksym Tarabara <tarabara.maksym@protonmail.com>
2025-08-10 18:43:46 +0300

    fix: fixing preload

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,15 @@
 import { render } from 'preact'
 import './index.css'
 import { App } from './app.tsx'
+import { preloadHomePageAssets, preloadAuthPageAssets } from '@/utils/globalPreloader'
+
+try {
+	const path = window.location.pathname;
+	if (path.startsWith('/auth')) {
+		preloadAuthPageAssets();
+	} else {
+		preloadHomePageAssets();
+	}
+} catch (_) { }
 
 render(<App />, document.getElementById('app')!)
--- frontend/src/utils/globalPreloader.ts
+++ frontend/src/utils/globalPreloader.ts
@@ -4,24 +4,47 @@ import authGif from '@/assets/lighthouse.gif';
 
 export function preloadHomePageAssets() {
 	const criticalImages = [nightPng, clouds];
-
+	const appended = new Set<string>();
 	criticalImages.forEach(src => {
-		const link = document.createElement('link');
-		link.rel = 'preload';
-		link.as = 'image';
-		link.href = src;
-		document.head.appendChild(link);
+		if (!src || appended.has(src)) {
+			return
+		};
+
+		if (!document.querySelector(`link[rel="preload"][as="image"][href="${src}"]`)) {
+			const link = document.createElement('link');
+			link.rel = 'preload';
+			link.as = 'image';
+			link.href = src;
+			document.head.appendChild(link);
+		}
+
+		const img = new Image();
+
+		img.decoding = 'async' as any;
+		img.src = src;
+		appended.add(src);
 	});
 }
 
 export function preloadAuthPageAssets() {
 	const criticalImages = [authGif];
+	const appended = new Set<string>();
 
 	criticalImages.forEach(src => {
-		const link = document.createElement('link');
-		link.rel = 'preload';
-		link.as = 'image';
-		link.href = src;
-		document.head.appendChild(link);
+		if (!src || appended.has(src)) {
+			return
+		};
+
+		if (!document.querySelector(`link[rel="preload"][as="image"][href="${src}"]`)) {
+			const link = document.createElement('link');
+			link.rel = 'preload';
+			link.as = 'image';
+			link.href = src;
+			document.head.appendChild(link);
+		}
+		const img = new Image();
+		img.decoding = 'async' as any;
+		img.src = src;
+		appended.add(src);
 	});
 }
